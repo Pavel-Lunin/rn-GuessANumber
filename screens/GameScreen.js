e39f51949b6a62aff0b6d263c57da51687cedd27
Лунин Павел <p.lunin@zarplata.ru>
2021-01-24 22:54:44 +0700

    Добавление игровых возможностей: подсказки и подтверждение

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, Alert } from 'react-native';
 
 import Card from '../components/Card';
 import NumberContainer from '../components/NumberContainer';
@@ -19,14 +19,46 @@ const GameScreen = (props) => {
   const [currentGuess, setCurrentGuess] = React.useState(
     generateRandomBetween(1, 100, props.userChoice)
   );
+  const currentLow = React.useRef(1);
+  const currenHigh = React.useRef(100);
+
+  const nextGuessHandler = (direction) => {
+    if (
+      (direction === 'lower' && currentGuess < props.userChoice) ||
+      (direction === 'greater' && currentGuess > props.userChoice)
+    ) {
+      Alert.alert("Don't lie", 'You know that this is wrong...', [
+        { text: 'Sorry!', style: 'cancel' }
+      ]);
+      return;
+    }
+    if (direction === 'lower') {
+      currenHigh.current = currentGuess;
+    } else {
+      currentLow.current = currentGuess;
+    }
+    const nextNumber = generateRandomBetween(currentLow.current, currenHigh.current, currentGuess);
+    setCurrentGuess(nextNumber);
+  };
+
+  let confirmedOutput;
+  if (props.userChoice === currentGuess) {
+    confirmedOutput = (
+      <Card style={styles.buttonContainer}>
+        <Text>ТА ДАААА!!! Красава</Text>
+      </Card>
+    );
+  }
+
   return (
     <View style={styles.screen}>
       <Text>Opponent's Guess</Text>
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card style={styles.buttonContainer}>
-        <Button title="LOWER" onPress={() => {}} />
-        <Button title="GREATER" onPress={() => {}} />
+        <Button title="МЕНЬШЕ" onPress={nextGuessHandler.bind(this, 'lower')} />
+        <Button title="БОЛЬШЕ" onPress={nextGuessHandler.bind(this, 'greater')} />
       </Card>
+      {confirmedOutput}
     </View>
   );
 };
