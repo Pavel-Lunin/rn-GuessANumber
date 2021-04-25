61aa1ad2f6045502a16e6926b399febcdda7d51a
Лунин Павел <p.lunin@zarplata.ru>
2021-04-25 22:02:57 +0700

    добавил шрифты и обновил expo

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 import Header from './components/Header';
 import GameOverScreen from './screens/GameOverScreen';
 import GameScreen from './screens/GameScreen';
 import StartGameScreen from './screens/StartGameScreen';
 
+const fetchFonts = () => {
+    return Font.loadAsync({
+        'open-sans-regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+    });
+};
+
 export default function App() {
     const [userNumber, setUserNumber] = React.useState();
     const [guessRounds, setGuessRounds] = React.useState(0);
+    const [dataLoaded, setDataLoaded] = React.useState(false);
+
+    if (!dataLoaded) {
+        return (
+            <AppLoading
+                startAsync={fetchFonts}
+                onFinish={() => setDataLoaded(true)}
+                onError={(err) => console.log(err)}
+            />
+        );
+    }
 
     const configureNewGameHandler = () => {
         setGuessRounds(0);
